5a65af1633702b3006428183a063ce526c747621
M. Pennella <michael.pennella@gmail.com>
2018-12-22 03:14:01 -0800

    Added route to find single article and populate comments

--- routes/routes.js
+++ routes/routes.js
@@ -81,8 +81,8 @@ module.exports = function(app) {
     app.get("/articles/:id", function(req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
-        // ..and populate all of the notes associated with it
-        .populate("note")
+        // ..and populate all of the Comments associated with it
+        .populate("comments")
         .then(function(dbArticle) {
         // If we were able to successfully find an Article with the given id, send it back to the client
         res.json(dbArticle);
