e6aef524a0794fba96a319e816f5855d9aa1a9dc
M. Pennella <michael.pennella@gmail.com>
2018-12-22 02:42:05 -0800

    Added framework for comment submission

--- public/js/index.js
+++ public/js/index.js
@@ -2,5 +2,11 @@ console.log("Javascript loaded");
 
 $(document).on("click",".commentBtn", function (event) {
     const button = $(this);
-    console.log(button.data("id"));
+    console.log(button.parent().data("id"));
+    button.siblings(".commentBox").toggle();
+})
+
+$(document).on("click",".postCmntBtn", function (event) {
+    const button = $(this);
+    console.log(button.parents(".articleCard").data("id"));
 })
