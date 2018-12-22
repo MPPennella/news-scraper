ea91f967951342a3dda344b8507de40f2d19d861
M. Pennella <michael.pennella@gmail.com>
2018-12-22 02:48:57 -0800

    Created Comment schema requiring commentText/author/date

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,32 @@
-module.exports = undefined;
+const mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+const Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+const CommentSchema = new Schema({
+    // `commentText` is required and of type String
+    commentText: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    // `author` is required and is of type String
+    author: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    datePosted: {
+        type: Date,
+        required: true
+    }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+const Comment = mongoose.model("Comment", CommentSchema);
+
+// Export the Comment model
+module.exports = Comment;
