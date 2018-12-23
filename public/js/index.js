23ff18a78b338b1cb16de641815ef274099931f4
M. Pennella <michael.pennella@gmail.com>
2018-12-22 22:04:20 -0800

    Added form submission for comments

--- public/js/index.js
+++ public/js/index.js
@@ -10,3 +10,17 @@ $(document).on("click",".postCmntBtn", function (event) {
     const button = $(this);
     console.log(button.parents(".articleCard").data("id"));
 })
+
+$(document).on("submit","form", function (event) {
+    event.preventDefault();
+    const form = $(this)
+
+    const newComment = {
+        commentText: $(this).find("[name=comment]").val().trim(),
+        author: "Test User".trim(),
+        datePosted: new Date()
+    }
+
+    const articleId = placeholder = "5c1dfd8054e4c31bfc8f92e6"
+    $.post(`./articles/comment/${articleId}`, newComment);
+})
